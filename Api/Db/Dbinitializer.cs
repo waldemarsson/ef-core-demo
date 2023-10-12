5d4f88b6c2fe4d52bd966b9e18d2cb9f975a8512
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2023-10-12 20:29:24 +0200

    Add projects

---
+++ Api/Db/Dbinitializer.cs
@@ -0,0 +1,37 @@
+﻿namespace Api.Db;
+
+public static class DbInitializer
+{
+    public static void Initialize(TodoContext context)
+    {
+        if (context.Todos.Any())
+        {
+            return;   // DB has been seeded
+        }
+
+        var students = new TodoDbModel[]
+        {
+            new TodoDbModel
+            {
+                Title = "Do something",
+                Description = "Some text about what to do",
+                IsChecked = false,
+            },
+            new TodoDbModel
+            {
+                Title = "Do something else ",
+                Description = "Some text about what to do else",
+                IsChecked = false,
+            },
+            new TodoDbModel
+            {
+                Title = "Something done",
+                Description = "Some text",
+                IsChecked = true,
+            }
+        };
+
+        context.Todos.AddRange(students);
+        context.SaveChanges();
+    }
+}
---
+++ Api/Entities/Todo.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.Entities;
+
+public class Todo
+{
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public bool IsChecked { get; init; }
+
+    public Todo(string? title, string? description, bool? isChecked)
+    {
+        Title = title ?? string.Empty;
+        Description = description ?? string.Empty;
+        IsChecked = isChecked ?? false;
+    }
+}
