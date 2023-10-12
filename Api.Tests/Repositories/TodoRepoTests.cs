bf4af613d29146d0c08d8b1fb0725b013dbb3a34
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2023-10-12 20:37:20 +0200

    Cleanup

--- Api.Tests/Repositories/TodoRepoTests.cs
+++ Api.Tests/Repositories/TodoRepoTests.cs
@@ -1,14 +1,7 @@
 ﻿using Api.Db;
 using Api.Repositories;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using NSubstitute;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Api.Tests.Repositories;
 
@@ -25,7 +18,7 @@ public class TodoRepoTests
                .UseSqlite($"Data Source={Path.Join(path, "todo.db")}")
                .Options;
 
-        var students = new TodoDbModel[]
+        var todos = new TodoDbModel[]
         {
             new TodoDbModel
             {
@@ -47,29 +40,27 @@ public class TodoRepoTests
             }
         };
 
-        using (var context = new TodoContext(dbContextOptions))
-        {
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-            context.Todos.AddRange(students);
-            context.SaveChanges();
-        }
+        using var context = new TodoContext(dbContextOptions);
+        
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        context.Todos.AddRange(todos);
+        context.SaveChanges();
     }
 
     [Fact]
     public async Task Should_ReturnAll_When_Requested()
     {
-        using (var context = new TodoContext(dbContextOptions))
-        {
-            // Arrange
-            var repo = new TodoRepo(context);
+        // Arrange
+        using var context = new TodoContext(dbContextOptions);
+        var repo = new TodoRepo(context);
 
-            // Act
-            var result = await repo.GetAll();
+        // Act
+        var result = await repo.GetAll();
 
-            // Assert 
-            result.Count.ShouldBe(3);
-        }
+        // Assert 
+        result.Count.ShouldBe(3);
+        
     }
 
     [Theory]
@@ -77,37 +68,33 @@ public class TodoRepoTests
     [InlineData(2, "Do something else", "Some text about what to do else", true)]
     public async Task Should_ReturnOne_When_Requested(int id, string expectedTitle, string expectedDescription, bool expectedIsCheched)
     {
-        using (var context = new TodoContext(dbContextOptions))
-        {
-            // Arrange
-            var repo = new TodoRepo(context);
+        // Arrange
+        using var context = new TodoContext(dbContextOptions);
+        var repo = new TodoRepo(context);
 
-            // Act
-            var result = await repo.FindOne(id);
+        // Act
+        var result = await repo.FindOne(id);
 
-            // Assert 
-            result.ShouldNotBeNull();
-            result.Title.ShouldBe(expectedTitle);
-            result.Description.ShouldBe(expectedDescription);
-            result.IsChecked.ShouldBe(expectedIsCheched);
-        }
+        // Assert 
+        result.ShouldNotBeNull();
+        result.Title.ShouldBe(expectedTitle);
+        result.Description.ShouldBe(expectedDescription);
+        result.IsChecked.ShouldBe(expectedIsCheched);
     }
 
     [Fact]
     public async Task Should_ReturnNull_When_EntityDoesNotExistsInDatabase()
     {
-        using (var context = new TodoContext(dbContextOptions))
-        {
-            // Arrange
-            var id = 10;
-            var repo = new TodoRepo(context);
+        // Arrange
+        var id = 10;
+        using var context = new TodoContext(dbContextOptions);
+        var repo = new TodoRepo(context);
 
-            // Act
-            var result = await repo.FindOne(id);
+        // Act
+        var result = await repo.FindOne(id);
 
-            // Assert 
-            result.ShouldBeNull();
-        }
+        // Assert 
+        result.ShouldBeNull();
     }
 
     [Theory]
@@ -115,19 +102,17 @@ public class TodoRepoTests
     [InlineData(3)]
     public async Task Should_Delete_When_UsingDeleteWithId(int id)
     {
-        using (var context = new TodoContext(dbContextOptions))
-        {
-            // Arrange
-            var repo = new TodoRepo(context);
+        // Arrange
+        using var context = new TodoContext(dbContextOptions);
+        var repo = new TodoRepo(context);
 
-            // Act
-            var result = await repo.Delete(id);
+        // Act
+        var result = await repo.Delete(id);
 
-            // Assert 
-            result.ShouldBe(1);
-            (await repo.FindOne(id)).ShouldBeNull();
-            (await repo.GetAll()).Count.ShouldBe(2);
-        }
+        // Assert 
+        result.ShouldBe(1);
+        (await repo.FindOne(id)).ShouldBeNull();
+        (await repo.GetAll()).Count.ShouldBe(2);
     }
 }
 
